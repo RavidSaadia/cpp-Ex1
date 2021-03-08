6eb64fd0604ee4f844f50b9be92081ce0e0dbde9
RavidSaadia <ravids1233@gmail.com>
2021-03-08 22:31:43 +0200

    end with part b' (building the snowman)

--- Test.cpp
+++ Test.cpp
@@ -4,7 +4,7 @@
  *
  * IMPORTANT: Please write more tests - the tests here are only for example and are not complete.
  *
- * AUTHORS: <Please write your names here>
+ * AUTHORS: <ravid saadia>
  *
  * Date: 2021-02
  */
@@ -16,19 +16,14 @@
 using namespace ariel;
 using namespace std;
 
-
+//const string[5] = {"",""};
 TEST_CASE("Good snowman code") {
-            CHECK(ariel::snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
-            CHECK(ariel::snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
-            CHECK(ariel::snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
-            CHECK(ariel::snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
-            CHECK(ariel::snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
-            CHECK(ariel::snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
-            CHECK(ariel::snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
-            CHECK(ariel::snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
-            CHECK(ariel::snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
-            CHECK(ariel::snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
-            CHECK(ariel::snowman(33232124) == string("_\n(.,.)\n( : )\n( : )"));
+
+            CHECK(ariel::snowman(11114411) == string("       \n _===_ \n (.,.) \n ( : ) \n ( : ) "));
+            CHECK(ariel::snowman(22221122) == string("  ___  \n ..... \n (o.o) \n<(] [)>\n (\" \") "));
+            CHECK(ariel::snowman(33332233) == string("   _   \n  /_\\  \n\\(O_O)/\n (> <) \n (___) "));
+            CHECK(ariel::snowman(44443344) == string("  ___  \n (_*_) \n (- -) \n/(   )\\\n (   ) "));
+            CHECK(ariel::snowman(12341234) == string("  ___  \n (_*_) \n (- -) \n/(   )\\\n (   ) "));
     /* Add more checks here */
 
 
@@ -40,17 +35,15 @@ TEST_CASE("Good snowman code") {
 
 TEST_CASE("Bad snowman code") {
 
-//    CHECK_THROWS(ariel::snowman(555));
     CHECK_THROWS(ariel::snowman(111111111));
     CHECK_THROWS(ariel::snowman(111111111));
-    CHECK_THROWS(ariel::snowman(111111111));
-    CHECK_THROWS(ariel::snowman(111111111));
-    CHECK_THROWS(ariel::snowman(111111111));
-    CHECK_THROWS(ariel::snowman(111111111));
-    CHECK_THROWS(ariel::snowman(111111111));
-    CHECK_THROWS(ariel::snowman(111111111));
-    CHECK_THROWS(ariel::snowman(111111111));
+    CHECK_THROWS(ariel::snowman(-11432432));
+    CHECK_THROWS(ariel::snowman(-19432432));
+    CHECK_THROWS(ariel::snowman(0));
+    CHECK_THROWS(ariel::snowman(00000000));
     CHECK_THROWS(ariel::snowman(555));
+    CHECK_THROWS(ariel::snowman(2223));
+    CHECK_THROWS(ariel::snowman(55234));
     /* Add more checks here */
 }
 
--- snowman.cpp
+++ snowman.cpp
@@ -3,42 +3,189 @@
 //
 #include "snowman.hpp"
 #include <string>
+#include <array>
 #include <iostream>
+//#include <iterator>
+//#include <algorithm>
 
 namespace ariel {
     using namespace std;
-    string  snowman(int input) {
+
+    string snowman(int input) {
+// input checks
         const int next_digit = 10;
         const int legal_digit_num = 8;
-
         int check = input;
-
         if (check < 0) { // check if the input is neg
-            cout<<"negative number!!!\n";
+            cout << "negative number!!!\n";
             throw std::exception();
-
         }
         for (int i = 0; i < legal_digit_num; ++i) { // simple check if the input is correct
-
-
-              if (check % next_digit > 4 || check % next_digit == 0) {
-              cout<< "digits are not 1-4 !!!\n";
-                  throw std::exception();
-              }
-
-
-                check = (int) (check / next_digit);
-
+            if (check % next_digit > 4 || check % next_digit == 0) {
+                cout << "digits are not 1-4 !!!\n";
+                throw std::exception();
+            }
+            check = (int) (check / next_digit);
         }
         if (check != 0) { // check if the input is more then 8 digits
-       cout<<"more than 8 digits!!!\n";
+            cout << "more than 8 digits!!!\n";
             throw std::exception();
 
         }
+// build a snow man!!!
+        int parts[8] = {0};
+        check = input;
+        for (int i = legal_digit_num - 1; i >= 0; --i) { // fill the parts array with the input
+            parts[i] = check % next_digit;
+            check = check / next_digit;
+        }
+        string ans;
 
+        switch (parts[0]) {  // hat
+            case 1:
+                ans = "       \n _===_ \n";
+                break;
+            case 2:
+                ans = "  ___  \n ..... \n";
+                break;
+            case 3:
+                ans = "   _   \n  /_\\  \n";
+                break;
+            case 4:
+                ans = "  ___  \n (_*_) \n";
+                break;
+            default:
+                ;
+        }
+        switch (parts[4]) { // left hand if up
+            case 1:
+                ans = ans + " ";
+                break;
+            case 2:
+                ans = ans + "\\";
+                break;
+            case 3:
+                ans = ans + " ";
+                break;
+            case 4:
+                ans = ans + " ";
+                break;
+        }
+        switch (parts[2]) {// left eye
+            case 1:
+                ans = ans + "(.";
+                break;
+            case 2:
+                ans = ans + "(o";
+                break;
+            case 3:
+                ans = ans + "(O";
+                break;
+            case 4:
+                ans = ans + "(-";
+                break;
+        }
+        switch (parts[1]) {// nose
+            case 1:
+                ans = ans + ",";
+                break;
+            case 2:
+                ans = ans + ".";
+                break;
+            case 3:
+                ans = ans + "_";
+                break;
+            case 4:
+                ans = ans + " ";
+                break;
+        }
+        switch (parts[3]) {// right eye
+            case 1:
+                ans = ans + ".)";
+                break;
+            case 2:
+                ans = ans + "o)";
+                break;
+            case 3:
+                ans = ans + "O)";
+                break;
+            case 4:
+                ans = ans + "-)";
+                break;
+        }
+        switch (parts[5]) { // right hand if up
+            case 1:
+                ans = ans + " \n";
+                break;
+            case 2:
+                ans = ans + "/\n";
+                break;
+            case 3:
+                ans = ans + " \n";
+                break;
+            case 4:
+                ans = ans + " \n";
+                break;
+        }
+        switch (parts[4]) { // left hand if down
+            case 1:
+                ans = ans + "<";
+                break;
+            case 2:
+                ans = ans + " ";
+                break;
+            case 3:
+                ans = ans + "/";
+                break;
+            case 4:
+                ans = ans + " ";
+                break;
+        }
+        switch (parts[6]) { // shirt
+            case 1:
+                ans = ans + "( : )";
+                break;
+            case 2:
+                ans = ans + "(] [)";
+                break;
+            case 3:
+                ans = ans + "(> <)";
+                break;
+            case 4:
+                ans = ans + "(   )";
+                break;
+        }
+        switch (parts[5]) { // right hand if down
+            case 1:
+                ans = ans + ">\n";
+                break;
+            case 2:
+                ans = ans + " \n";
+                break;
+            case 3:
+                ans = ans + "\\\n";
+                break;
+            case 4:
+                ans = ans + " \n";
+                break;
+        }
+        switch (parts[7]) { // basic
+            case 1:
+                ans = ans +" ( : ) " ;
+                break;
+            case 2:
+                ans = ans +" (\" \") " ;
+                break;
+            case 3:
+                ans = ans +" (___) " ;
+                break;
+            case 4:
+                ans = ans +" (   ) " ;
+                break;
+        }
 
-        string s = "";
-        return s;
+//        cout<<ans;
+        return ans;
     }
 
 }
